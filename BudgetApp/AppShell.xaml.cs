d7c7c1c6c7dea09156537a4b26dfcdc579c897dc
Samuel M B Winter <samuel.m.b.winter@protonmail.com>
2021-09-20 14:18:06 +0100

    Initial Commit

---
+++ BudgetApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using BudgetApp.ViewModels;
+using BudgetApp.Views;
+using Xamarin.Forms;
+
+namespace BudgetApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
